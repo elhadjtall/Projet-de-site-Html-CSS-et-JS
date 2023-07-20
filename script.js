0bd12c685f8f29d05fb8be9143ac00d4b7168776
unknown <elhadjoumartall10@gmail.com>
2023-07-20 15:53:31 +0200

    mon projet de site

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let words = document.querySelectorAll(".word");
 words.forEach((word)=>{
     let letters = word.textContent.split("");
     word.textContent="";
-    letters.forEach((letters)=>{
+    letters.forEach((letter)=>{
         let span = document.createElement("span");
         span.textContent = letter;
         span.className = "letter";
         word.append(span);
     });
 });
-let cur
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].computedStyleMap.opacity = "1";
