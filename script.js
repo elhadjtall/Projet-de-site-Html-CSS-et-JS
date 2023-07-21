927e1b9772f940c2e23deb8bc7f232b8625f4224
unknown <elhadjoumartall10@gmail.com>
2023-07-21 13:17:28 +0200

    mon projet de site

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
-    let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter)=>{
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
-    });
-});
-let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
-words[currentWordIndex].computedStyleMap.opacity = "1";
+// Scroll section active link ================================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a'); 
+// ce code sélectionne tous les éléments <section> de la page et
+//  les stocke dans la variable sections, puis il sélectionne tous les liens <a> 
+// qui se trouvent dans les balises <nav> du <header> et les stocke dans la variable navLinks.
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=')
+            })
+        }
+    })
+}
