22aba9c5af230f2c15702ffe65c191b103e85c03
unknown <elhadjoumartall10@gmail.com>
2023-07-21 16:16:39 +0200

    mon projet de site

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
-        }
-    })
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
 }
