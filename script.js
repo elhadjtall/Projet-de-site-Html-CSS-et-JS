fa56ede4e8a30a71c91bb9420ae02f8c5ca1a404
unknown <elhadjoumartall10@gmail.com>
2023-07-22 12:09:09 +0200

    mon projet de site

--- script.js
+++ script.js
@@ -52,4 +52,6 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-content, .heading, .portofolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
