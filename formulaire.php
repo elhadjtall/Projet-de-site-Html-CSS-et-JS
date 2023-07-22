d9ccb9d7fe6ce4f9c2ebf556bf7b91ac4a9a3772
unknown <elhadjoumartall10@gmail.com>
2023-07-22 14:11:59 +0200

    mon projet de site

---
+++ formulaire.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multi Step Form using HTML CSS JS </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="login-box">
+            <form action="#">
+                <div class="login-tilte">
+                    <h2>Connexion</h2>
+                    <p>PLease login to use the platform</p>
+                </div>
+                <div class="page">
+                    <div class="input-box">
+                        <input type="text" name="" id="">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
