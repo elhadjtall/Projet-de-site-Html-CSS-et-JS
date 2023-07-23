f90540b4ed6a5cff7ab44165432e92257f4c377f
unknown <elhadjoumartall10@gmail.com>
2023-07-23 14:29:39 +0200

    mon projet de site

--- formulaire/script.js
+++ formulaire/script.js
@@ -6,6 +6,8 @@ const btnBack = document.querySelector('.btn-back');
 const checkboxPass = document.querySelector('.checkbox-pass');
 const passwordInput = document.querySelector('.password');
 const loginTitle = document.querySelector('.loginTitle-text');
+const userEmail = document.querySelector('.user-email');
+const emailInput = document.querySelector('.email')
 
 // ce code permet de passer en avant avec un button 
 btnNext.onclick = (e) => {
@@ -13,12 +15,14 @@ btnNext.onclick = (e) => {
     pageBox.classList.add('active-pass');
     setTimeout(() => passwordInput.focus(), 500);
     loginTitle.innerHTML = 'Welcome'; //ce code affiche un text de bienvenu sur la page
+    userEmail.innerHTML = emailInput.value; // ce code permet de faire apparaitre l'email sur la barre du mot de passe
 };
 //  Ce code permet de retourner en arrière avec un button
 btnBack.onclick = (e) => {
     e.preventDefault();
     pageBox.classList.remove('active-pass');
     loginTitle.innerHTML = 'Connexion';
+    userEmail.innerHTML = 'Please login to use the platform';
 };
 
 // Ce code permet de checker le mot de passe dans le champs input en precisant de quel type
