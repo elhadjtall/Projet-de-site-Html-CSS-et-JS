e9dc5d6426c320636a325d6ac95fb5128393162f
unknown <elhadjoumartall10@gmail.com>
2023-07-23 13:52:25 +0200

    mon projet de site

--- formulaire/script.js
+++ formulaire/script.js
@@ -1,14 +1,17 @@
+// Les contantes creer comme variable
+
 const pageBox = document.querySelector('.page-box');
 const btnNext = document.querySelector('.btn-next');
 const btnBack = document.querySelector('.btn-back');
 
-
+// ce code permet de passer en avant avec un button 
 btnNext.onclick = (e) => {
     e.preventDefault();
     pageBox.classList.add('active-pass');
 };
-
+//  Ce code permet de retourner en arrière avec un button
 btnBack.onclick = (e) => {
     e.preventDefault();
     pageBox.classList.remove('active-pass');
 };
+
