d04ffe7d268555619415ffdbc25d9ca021572943
unknown <elhadjoumartall10@gmail.com>
2023-07-23 13:47:47 +0200

    mon projet de site

--- formulaire/script.js
+++ formulaire/script.js
@@ -1,8 +1,14 @@
 const pageBox = document.querySelector('.page-box');
 const btnNext = document.querySelector('.btn-next');
+const btnBack = document.querySelector('.btn-back');
 
 
 btnNext.onclick = (e) => {
     e.preventDefault();
     pageBox.classList.add('active-pass');
+};
+
+btnBack.onclick = (e) => {
+    e.preventDefault();
+    pageBox.classList.remove('active-pass');
 };
