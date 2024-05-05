e0dc6923f8d5ce2b503328b23d0a9eb04347c451
unknown <elhadjoumartall10@gmail.com>
2024-05-05 23:38:45 +0200

    Modification du code

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 // typed js ============================== */
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Developpeur Web', 'Web Design'],
+    strings: ['Developpeur Web Junior', 'Web Design'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
