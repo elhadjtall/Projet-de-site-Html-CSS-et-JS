590f633dd051996b30abeb092eb4caff8937f0f5
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-10-31 00:56:35 +0100

    AJout du slider projet

--- script.js
+++ script.js
@@ -66,3 +66,16 @@ const typed = new Typed('.multiple-text', {
     loop: true
 });
 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
